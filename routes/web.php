d22f122946dc1c9209f48918f85b473f5ccb7af3
Abhishek Chaudhary <iamdev.abhishek@gmail.com>
2024-04-18 10:23:00 +0100

    composition create re routing and website name changes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/audio/create');
+    return redirect()->route('composition.create');
 });
 
 Auth::routes();
